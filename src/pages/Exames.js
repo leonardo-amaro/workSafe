d785e192e25a73a4cb94e82e51df112afc136dae
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2023-11-21 20:54:45 -0300

    removing unused imports

--- src/pages/Exames.js
+++ src/pages/Exames.js
@@ -2,30 +2,13 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { HeaderView } from '../components/headerview'
 import { Title } from '../components/title';
 import { Card } from '../components/card';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
 
 export function Exames({ navigation }) {
-  const [exames, setExames] = useState([]);
-
-  useEffect(() => {
-    loadExams();
-  }, []);
-
-  const loadExams = async () => {
-    const result = await axios.get("http://localhost:8080/allexams");
-    setExames(result.data);
-  };
-
   return (
     <View style={styles.container}>
       <HeaderView />
       <Title>Exames</Title>
-      {exames.map( exame => (
-        <Card
-          nome={exame.exam_name}
-        />
-      ) )}
+      <Card />
       <View style={styles.buttonArea}>
         <TouchableOpacity style={styles.button} onPress={ () => navigation.navigate('ModalExam') }>
           <Text style={styles.buttonText}>
--- src/pages/Funcionarios.js
+++ src/pages/Funcionarios.js
@@ -2,30 +2,13 @@ import { Text, TouchableOpacity, StyleSheet, View } from 'react-native';
 import { HeaderView } from '../components/headerview'
 import { Title } from '../components/title';
 import { Card } from '../components/card';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
 
 export function Funcionarios({ navigation }) {
-  const [funcionarios, setFuncionarios] = useState([]);
-  
-  useEffect(() => {
-    loadFuncionarios();
-  }, []);
-
-  const loadFuncionarios = async () => {
-    const result = await axios.get("http://localhost:8080/allemployees");
-    setFuncionarios(result.data);
-  };
-
   return (
     <View style={styles.container}>
       <HeaderView />
       <Title>Funcionários</Title>
-      {funcionarios.map( funcionario => (
-        <Card
-          nome={funcionario.employee_name}
-        />
-      ) )}
+      <Card />
       <View style={styles.buttonArea}>
         <TouchableOpacity style={styles.button} onPress={ () => navigation.navigate('ModalFuncionario') }>
           <Text style={styles.buttonText}>
